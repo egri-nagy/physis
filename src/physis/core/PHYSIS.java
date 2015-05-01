22c94f69487f77cf73824d8926db868861146cf4
Dersu Uzala <attila@egri-nagy.hu>
2015-05-01 20:13:30 +1000

    import optimization, indentation, for -> fore each

--- src/physis/core/PHYSIS.java
+++ src/physis/core/PHYSIS.java
@@ -8,16 +8,13 @@
  */
 package physis.core;
 
-import physis.visualisation.Viewer;
-import physis.core.triggers.Triggers;
 import physis.core.statistics.Statistics;
 import physis.core.statistics.StatisticsFactory;
-import physis.core.virtualmachine.InstructionSet;
-import physis.core.virtualmachine.GeneticCodeTape;
+import physis.core.triggers.Triggers;
 import physis.core.virtualmachine.GeneticCodeTapeFactory;
-import physis.core.task.TaskLibrary;
-
+import physis.core.virtualmachine.InstructionSet;
 import physis.log.Log;
+import physis.visualisation.Viewer;
 
 import java.io.File;
 
@@ -41,7 +38,7 @@ public final class PHYSIS extends Thread{
      * The default environment is EnvironmentImpl is instantiated by PHYSIS.
      */
     public static Environment environment;
-    
+
     /** The name of the file containing the configuration parameters (with full path). */
     private static String config_file = "";
     /** The instance of the system. */
@@ -58,116 +55,115 @@ public final class PHYSIS extends Thread{
     private long updatecount = 1;
     /** The maximum number of possible updates is defined in the configuration file. */
     private long max_updates;
-    
+
     /**
      * This is the main entry point.
      */
     public static void main(String[] args) {
         //sanity check - the only argument we need the (full) name of the configuration file.
         if ( (args.length != 1) || (!(new File(args[0]).isFile())) ){
-	    System.out.println("USAGE: java physis.core.PHYSIS configuration_file");
-	    System.exit(-1);
+            System.out.println("USAGE: java physis.core.PHYSIS configuration_file");
+            System.exit(-1);
         }
         Log.status(Log.getFullTimeStamp());
         PHYSIS.setConfigFileName(args[0]);
         PHYSIS physis = PHYSIS.getInstance();
         physis.start();
     }
-    
+
     /**
      * Sets the configuration file. After this the system can be instantiated.
      */
     public static void setConfigFileName(String filename){
         config_file = filename;
-	Log.status("CONFIGURATION FILE: " + config_file);
+        Log.status("CONFIGURATION FILE: " + config_file);
     }
-    
+
     /** This returns an instance of the system if the full path of the configuration file is correctly set. */
     public static PHYSIS getInstance(){
         if (physis != null){
         }else{
-	    if ("".equals(config_file)){
-		Log.error("Filename of the configfile isn't set. Unable to start the core system.");
-		System.exit(1);
-	    }
-	    else{
-		physis = new PHYSIS(config_file);
-	    }
+            if ("".equals(config_file)){
+                Log.error("Filename of the configfile isn't set. Unable to start the core system.");
+                System.exit(1);
+            }
+            else{
+                physis = new PHYSIS(config_file);
+            }
         }
         return physis;
     }
-    
+
     /** The thread's run method. It's a cycle in which the system parts are updated. */
+    @Override
     public void run(){
-	//it should be set to minimum priority in order to get the GUI work properly
-	setPriority(Thread.MIN_PRIORITY);
-	
-	triggers = new Triggers();
-	
-	//just some message saying everything is ok
-	Log.printMemoryInfo();
-	Log.status("PHYSIS system is ready to run " + max_updates + " updates.\n");
-	try{
-	    while(updatecount < max_updates){
-		
-		//updating the state of the population
-		population.update();
-		
-		//checking for triggers to be executed
-		physis.triggers.processActualTriggers(physis.updatecount);
-		
-		//gathering statistical  information
-		physis.statistics.gatherInformation(updatecount);
-		
-		//refreshing the viewers
-		for (int i = 0; i < viewers.length; i ++){
-		    viewers[i].refresh();
-		}
-		
-		//Log.status(physis.updatecount + ". update finished.");
-		//Log.printMemoryInfo();
-		updatecount++;
-		
-		
-	    }
-	}catch (Exception e){
-	    Log.printMemoryInfo();
-	    Log.error(e);
-	}
+        //it should be set to minimum priority in order to get the GUI work properly
+        setPriority(Thread.MIN_PRIORITY);
+
+        triggers = new Triggers();
+
+        //just some message saying everything is ok
+        Log.printMemoryInfo();
+        Log.status("PHYSIS system is ready to run " + max_updates + " updates.\n");
+        try{
+            while(updatecount < max_updates){
+
+                //updating the state of the population
+                population.update();
+
+                //checking for triggers to be executed
+                physis.triggers.processActualTriggers(physis.updatecount);
+
+                //gathering statistical  information
+                physis.statistics.gatherInformation(updatecount);
+
+                //refreshing the viewers
+                for (Viewer viewer : viewers) {viewer.refresh();}
+
+                //Log.status(physis.updatecount + ". update finished.");
+                //Log.printMemoryInfo();
+                updatecount++;
+
+
+            }
+        }catch (Exception e){
+            Log.printMemoryInfo();
+            Log.error(e);
+        }
         Log.status("THE END");
-        
+
     }
-    
+
     private PHYSIS(String config_file){
         Log.status("PHYSIS core system is starting...");
-	
+
         //first loading the parameters
-	Configuration.init(config_file);
-	
-	//instantiating the environment
+        Configuration.init(config_file);
+
+        //instantiating the environment
         environment = new EnvironmentImpl();
-        
+
         //instantiating InstructionSet
         InstructionSet.getInstance();
 
-	GeneticCodeTapeFactory.init();
-	
+        GeneticCodeTapeFactory.init();
+
         //instantiating the system
         population = new Population();
-        
-	//statistics instatiating
-	statistics = StatisticsFactory.getStatistics(population);
-        
-	//Setting the maximum number of updates
+
+        //statistics instantiating
+        statistics = StatisticsFactory.getStatistics(population);
+
+        //Setting the maximum number of updates
         max_updates = Configuration.getMaxNumberOfUpdates();
     }
-    
+
     /** Returns the instance's population. */
     public Population getPopulation(){ return population; }
-    
+
     /** Returns the number of the current update cycle. */
     public long getUpdateCount() { return updatecount; }
-    
+
     /** The viewers can be set dynamically. */
     public void setViewers(Viewer[] viewers){
         this.viewers = viewers;
